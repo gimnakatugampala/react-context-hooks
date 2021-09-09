c206bf10bb6c0a5834ca6f6221f2f42a8fa23b6b
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-08 18:39:48 -0700

    basic template booklist & navbar

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BookList = () => {
+    return (
+        <div className="book-list">
+            <ul>
+                <li>The Way of Kings</li>
+                <li>The Name of the wind</li>
+                <li>The Final Empire</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList
